1a2613368580ba9f65a4951ab35b1e755a7e4b28
duyguolmez <dolmez@havelsan.com.tr>
2022-06-01 08:37:02 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"github.com/masterzen/winrm"
+	"golang.org/x/text/encoding/unicode"
+	"os"
+	"strconv"
+	"strings"
+)
+
+//InitWinRMShell InitWinRMShell
+func InitWinRMShell(username string, password string, ipAddress string, port string) (*winrm.Client, error) {
+	intPort, _ := strconv.Atoi(port)
+	endpoint := winrm.NewEndpoint(ipAddress, intPort, true, true, nil, nil, nil, 0)
+	params := winrm.DefaultParameters
+	params.TransportDecorator = func() winrm.Transporter { return &winrm.ClientNTLM{} }
+
+	client, err := winrm.NewClientWithParameters(endpoint, username, password, params)
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
+
+}
+
+//VerifyWinRM VerifyWinRM
+func VerifyWinRM(username string, password string, ipAddress string, port string) bool {
+	intPort, _ := strconv.Atoi(port)
+	endpoint := winrm.NewEndpoint(ipAddress, intPort, true, true, nil, nil, nil, 0)
+
+	params := winrm.DefaultParameters
+	params.TransportDecorator = func() winrm.Transporter { return &winrm.ClientNTLM{} }
+
+	client, err := winrm.NewClientWithParameters(endpoint, username, password, params)
+	if err != nil {
+		return false
+	}
+
+	stdout, _, _, _ := client.RunWithString("hostname", "")
+	if strings.TrimSpace(stdout) == "" {
+		return false
+	}
+	return true
+}
+
+func main() {
+	// Credentials
+	username := "testuser"
+	password := "securepassword"
+	ip := "securepassword"
+	port := "5986"
+	command := "hostname"
+
+	// Connect
+	connection, _ := InitWinRMShell(username, password, ip, port)
+	sdtcheck := VerifyWinRM(username, password, ip, port)
+
+	if sdtcheck == false {
+		fmt.Println("Bağlantı sağlanamadı")
+		os.Exit(1)
+	}
+
+	// Run command
+	encoder := unicode.UTF16(unicode.LittleEndian, unicode.IgnoreBOM).NewEncoder()
+	encoded, _ := encoder.String(command)
+	command = base64.StdEncoding.EncodeToString([]byte(encoded))
+
+	stdout, stderr, _, err := connection.RunWithString("powershell.exe -encodedCommand "+command, "")
+	// stdout, stderr, _, err := connection.RunWithString("powershell.exe -encodedCommand "+command, "")
+	fmt.Println(stdout)
+	fmt.Println(stderr)
+	fmt.Println(err)
+
+}
